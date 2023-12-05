8f0afcb31be34a4fc5249eeb9369ec174e9c192e
jasminew13 <jasminewu101@gmail.com>
2023-12-05 10:13:47 -0500

    gfs

--- api/js/main.js
+++ api/js/main.js
@@ -1,13 +1,15 @@
 import '../css/style.css'
 
+const black = `https://cataas.com/cat/black`;
+const white = `https://cataas.com/cat/white`;
+const ginger = `https://cataas.com/cat/ginger`;
+const tabby = `https://cataas.com/cat/tabby`;
 
 
-//const URLP = `pokemon/${search}`
-//async asyncronous 
-/* async function getData(URL){
+async function getData(white){
     try {
         //requesting a response REST API's
-        const response = await fetch(URL);
+        const response = await fetch(white);
         if (response.status != 200) {
 
             throw new Error (response.statusText);
@@ -20,10 +22,32 @@ import '../css/style.css'
         document.querySelector("h1").textContent = "nono"
     }
 }
-getData(URL); */
+getData(white); 
 
-const black = `https://cataas.com/cat/black`;
-const white = `https://cataas.com/cat/white`;
-const ginger = `https://cataas.com/cat/ginger`;
-const tabby = `https://cataas.com/cat/tabby`;
-console.log(ginger)
+
+
+
+async function addCards (arr){
+    arr.forEach((s) => {
+      DOMselectors.container.insertAdjacentHTML("afterend", 
+      `<div class="card">
+      <h2 class="albumname"> ${s.name}</h2>
+      <h2 class="artistname"> ${s.artist}</h2>
+      <img src="${s.image}" alt="" class="image">
+      <h3 class="price">${s.price}</h3>
+  </div>`
+  )})};
+  
+
+function clearscreen(){
+  DOMselectors.container.innerHTML= "";
+};
+
+let values = document.querySelectorAll(".pop, .indiepop, .rnb");
+
+values.forEach((value) => value.addEventListener("click", function(){
+  let type = value.textContent;
+  let newArr = cats.filter((cat) => cat.genre === type);
+  clearscreen();
+  addCards(newArr);
+}));
